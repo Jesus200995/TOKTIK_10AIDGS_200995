1d4cc32052dc12e711fbde4f051fbcdf952bcd39
Jesus R. Gomez <jesus.riosgomez@utxicotepec.edu.mx>
2024-11-29 11:20:56 -0600

    Practica 25
    Se modifico el sonido de los videos en la App

--- toktik_200995/lib/presentation/widgets/video/fullscreen_player.dart
+++ toktik_200995/lib/presentation/widgets/video/fullscreen_player.dart
@@ -3,15 +3,13 @@ import 'package:toktik_200995/presentation/widgets/video/video_background.dart';
 import 'package:video_player/video_player.dart';
 
 class FullScreenPlayer extends StatefulWidget {
-
   final String videoUrl;
   final String caption;
 
   const FullScreenPlayer({
     super.key,
-    required
-    this.videoUrl,
-    required this.caption
+    required this.videoUrl,
+    required this.caption,
   });
 
   @override
@@ -19,18 +17,20 @@ class FullScreenPlayer extends StatefulWidget {
 }
 
 class _FullScreenPlayerState extends State<FullScreenPlayer> {
-
   late VideoPlayerController controller;
 
   @override
   void initState() {
     super.initState();
 
-  controller = VideoPlayerController.asset( widget.videoUrl )
-    ..setVolume(0)
-    ..setLooping(true)
-    ..play();
-
+    // Cambiar el controlador para permitir audio
+    controller = VideoPlayerController.asset(widget.videoUrl)
+      ..setVolume(1.0) // Ajuste del volumen a máximo
+      ..setLooping(true)
+      ..initialize().then((_) {
+        setState(() {}); // Refresca la UI tras la inicialización
+        controller.play();
+      });
   }
 
   @override
@@ -39,73 +39,55 @@ class _FullScreenPlayerState extends State<FullScreenPlayer> {
     super.dispose();
   }
 
-
   @override
   Widget build(BuildContext context) {
-
-    return FutureBuilder(
-      future: controller.initialize(),
-      builder: (context, snapshot) {
-        if ( snapshot.connectionState != ConnectionState.done ){
-          return const Center( child: CircularProgressIndicator( strokeWidth: 2 ));
-        }
-
-        return GestureDetector(
-          onTap: () {
-            if ( controller.value.isPlaying ) {
-              controller.pause();
-              return;
-            }
-            controller.play();
-            
-          },
-          child: AspectRatio(
-            aspectRatio: controller.value.aspectRatio,
-            child: Stack(
-              children: [
-        
-                VideoPlayer(controller),
-        
-                // Gradiente
-                VideoBackground(
-                  stops: const [0.8,1.0],
-                ),
-        
-                // Texto
-                Positioned(
-                  bottom: 50,
-                  left: 20,
-                  child: _VideoCaption( caption: widget.caption )
-                ),
-        
-        
-              ],
+    return controller.value.isInitialized
+        ? GestureDetector(
+            onTap: () {
+              if (controller.value.isPlaying) {
+                controller.pause();
+              } else {
+                controller.play();
+              }
+            },
+            child: AspectRatio(
+              aspectRatio: controller.value.aspectRatio,
+              child: Stack(
+                children: [
+                  VideoPlayer(controller),
+                  VideoBackground(
+                    stops: const [0.8, 1.0],
+                  ),
+                  Positioned(
+                    bottom: 50,
+                    left: 20,
+                    child: _VideoCaption(caption: widget.caption),
+                  ),
+                ],
+              ),
             ),
-          ),
-        );
-
-      },
-    );
+          )
+        : const Center(child: CircularProgressIndicator(strokeWidth: 2));
   }
 }
 
-
 class _VideoCaption extends StatelessWidget {
-
   final String caption;
 
-
-  const _VideoCaption({super.key, required this.caption });
+  const _VideoCaption({super.key, required this.caption});
 
   @override
   Widget build(BuildContext context) {
-
     final size = MediaQuery.of(context).size;
     final titleStyle = Theme.of(context).textTheme.titleLarge;
 
     return SizedBox(
       width: size.width * 0.6,
-      child: Text( caption, maxLines: 2, style: titleStyle ),
+      child: Text(
+        caption,
+        maxLines: 2,
+        style: titleStyle,
+      ),
     );
   }
 }
